d5c7932294c5cca12442faae178fee6f3a0c3a9a
dsfrantsev <fds@avito.ru>
2023-12-16 16:10:30 +0300

    Header and Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles/main.css"
 
-import sun from  "./img/icons/sun.svg"
-import moon from "./img/icons/moon.svg"
+import Navbar from "./components/navbar/Navbar"
+import Header from "./components/header/Header"
 
 import vk from "./img/icons/vk.svg"
 import instagram from "./img/icons/instagram.svg"
@@ -12,37 +12,12 @@ import linked from "./img/icons/linkedIn.svg"
 function App() {
   return (
     <div className="App">
-      <nav className="nav">
-        <div className="container">
-            <div className="nav-row">
-                <a href="./index.html" className="logo"><strong>Freelancer</strong> portfolio</a>
+      
+      <Navbar/>
+      <Header/>
+      
 
-                <button className="dark-mode-btn">
-                    <img src={sun} alt="Light mode" className="dark-mode-btn__icon"/>
-                    <img src={moon} alt="Dark mode" className="dark-mode-btn__icon"/>
-                </button>
-
-                <ul className="nav-list">
-                    <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-                    <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-                    <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <header className="header">
-        <div className="header__wrapper">
-            <h1 className="header__title">
-                <strong>Hi, my name is <em>Danil</em></strong><br/>
-                a frontend developer
-            </h1>
-            <div className="header__text">
-                <p>with passion for learning and creating.</p>
-            </div>
-            <a href="#!" className="btn">Download CV</a>
-        </div>
-    </header>
+    
 
     <main className="section">
         <div className="container">
